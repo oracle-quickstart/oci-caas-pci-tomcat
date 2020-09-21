e6e221e5660075f744c1e3363a7d78c5683e765d
Jon DeCamp <jon.decamp@oracle.com>
2020-09-21 10:51:57 -0700

    Move wazuh template render override to the end

--- recipes/base.rb
+++ recipes/base.rb
@@ -25,10 +25,3 @@ end
 service 'chronyd' do
   action :enable
 end
-
-template "#{node['ossec']['dir']}/etc/shared/agent.conf" do
-  source 'wazuh_local_files.conf'
-  owner 'root'
-  group 'ossec'
-  action :create
-end
